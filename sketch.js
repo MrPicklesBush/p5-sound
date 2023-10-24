0d89ffd0168edb22ad7f0d5966a5e4b89aa2c450
MrPicklesBush <bush.nguyen02@gmail.com>
2023-10-24 08:35:43 -0700

    preload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var song;
+var slider;
+
+function preload() {
+  song = loadSound("gods.mp3")
+}
+function setup() {
+  createCanvas(400, 400);
+  slider = createSlider(0, 1, .1, 0.01)
+  song.play();
+}
+
+function draw() {
+  background(220);
+  song.setVolume(slider.value());
+}
